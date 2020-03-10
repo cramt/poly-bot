ec91f61c9b77c100eaec100d18ddc84636d4b8ed
Alexandra Østermark <alex.cramt@gmail.com>
2020-03-10 22:16:43 +0100

    moved reset db into schema setup test

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -27,36 +27,33 @@ describe('Database setup', () => {
         const string = Math.random().toString(36).substring(4);
         assert.eventually.equal(client.query("SELECT $1", [string]).then(x => x.rows[0]["?column?"]), string)
     })
-
-    it('Reset database', async () => {
-        try {
-            await client.query(`SELECT pid, pg_terminate_backend(pid) 
-            FROM pg_stat_activity 
-            WHERE datname = current_database() AND pid <> pg_backend_pid();`)
-            if (await (await client.query("SELECT 1 FROM pg_database WHERE datname = '_'")).rowCount === 0) {
-                await client.query("CREATE DATABASE _");
-            }
-            await client.end();
-            const thisDbConfig = JSON.parse(JSON.stringify(dbConfig)) as typeof dbConfig
-            thisDbConfig.database = "_"
-            client = new Client(thisDbConfig)
-            await client.connect();
-
-            await client.query("REVOKE CONNECT ON DATABASE " + SECRET.DB_NAME + " FROM public");
-            await client.query("SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '" + SECRET.DB_NAME + "';");
-            await client.query("DROP DATABASE IF EXISTS " + SECRET.DB_NAME)
-            await client.query("CREATE DATABASE " + SECRET.DB_NAME)
-            await client.end()
-            client = new Client(dbConfig)
-            await client.connect()
-            await client.query("DROP DATABASE IF EXISTS _")
+    
+    it("Schema setup", async () => {
+        await client.query(`SELECT pid, pg_terminate_backend(pid) 
+        FROM pg_stat_activity 
+        WHERE datname = current_database() AND pid <> pg_backend_pid();`)
+        if (await (await client.query("SELECT 1 FROM pg_database WHERE datname = '_'")).rowCount === 0) {
+            await client.query("CREATE DATABASE _");
         }
-        catch (e) { console.log(e) }
+        await client.end();
+        const thisDbConfig = JSON.parse(JSON.stringify(dbConfig)) as typeof dbConfig
+        thisDbConfig.database = "_"
+        client = new Client(thisDbConfig)
+        await client.connect();
+
+        await client.query("REVOKE CONNECT ON DATABASE " + SECRET.DB_NAME + " FROM public");
+        await client.query("SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '" + SECRET.DB_NAME + "';");
+        await client.query("DROP DATABASE IF EXISTS " + SECRET.DB_NAME)
+        await client.query("CREATE DATABASE " + SECRET.DB_NAME)
+        await client.end()
+        client = new Client(dbConfig)
+        await client.connect()
+        await client.query("DROP DATABASE IF EXISTS _")
 
         assert.eventually.deepEqual(client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").then(x => x.rows), [])
-    })
 
-    it("Schema setup", async () => {
+
+
         await setupSchema(client)
         assert.eventually.equal(client.query("SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'info'").then(x => x.rows[0].count), "1")
     })
