a503f4b2b3865a6c465a338f0a76234b9467a5a7
Alexandra Østermark <alex.cramt@gmail.com>
2020-03-13 15:58:28 +0100

    created 2 subclasses for User, GuildUser and DiscordUser

--- tests/db/users.test.ts
+++ tests/db/users.test.ts
@@ -1,7 +1,7 @@
 import chaiAsPromised from 'chai-as-promised';
 import * as chai from 'chai';
 import { users, openDB, genderIntToString } from '../../src/db';
-import { User } from '../../src/User';
+import { User, GuildUser } from '../../src/User';
 
 chai.use(chaiAsPromised)
 const assert = chai.assert
@@ -14,28 +14,29 @@ describe("Database User", () => {
     let name = Math.random().toString(36).substring(4)
     let gender = genderIntToString[Math.floor(Math.random() * Object.getOwnPropertyNames(genderIntToString).length)]
     let guild = Math.random().toString(36).substring(4)
-    let user = new User(name, gender, guild, null, null, null)
+    let guildUser = new GuildUser(name, gender, null, null, guild)
 
     it("Create user", async () => {
-        await assert.eventually.isTrue(users.add(user))
-        assert.isNotNull(user.id)
+        await assert.eventually.isTrue(users.add(guildUser))
+        assert.isNotNull(guildUser.id)
     })
 
     it("Get by username", async () => {
-        assert.deepEqual(await users.getByUsername(user.name, user.guildId!, []), [user])
+        assert.deepEqual(await users.getByUsername(guildUser.name, guildUser.guildId!, []), [guildUser])
     })
 
     let discord = Math.random().toString(36).substring(4)
 
+    let discordUser = guildUser.toDiscordUser(discord)
+
     it("Set discord id", async () => {
-        user.discordId = discord
-        user.guildId = null
-        assert.isTrue(await users.update(user))
+        assert.isTrue(await users.update(discordUser))
     })
 
     it("Delete by discord id", async () => {
-        assert.isTrue(await users.deleteByDiscord(user.discordId!))
-        assert.isNull(await users.get(user.id!))
+        assert.isTrue(await users.deleteByDiscord(discordUser.discordId!))
+        assert.isNull(await users.get(guildUser.id!))
+        assert.isNull(await users.get(discordUser.id!))
     })
 })
 
