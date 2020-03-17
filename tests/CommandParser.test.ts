301557f799adc85e8752ed49971501c7bc4deeaf
Lucca Christiansen <rkaasgaard@gmail.com>
2020-03-17 10:34:51 +0100

    test string excluded arguments, started user argumetns

--- tests/CommandParser.test.ts
+++ tests/CommandParser.test.ts
@@ -1,10 +1,13 @@
 import chaiAsPromised from 'chai-as-promised';
 import * as chai from 'chai';
 import * as sinon from 'sinon';
-import {User} from 'discord.js';
+import * as PolyUser from '../src/User'
+import { User } from 'discord.js';
 import * as Commands from '../src/commands';
 import * as CommandParser from '../src/Command';
-import {client} from '../src/index';
+import { client } from '../src/index';
+import { users } from '../src/db';
+import { ArgumentError } from '../src/Command';
 
 var cmd = Commands.commands;
 chai.use(chaiAsPromised)
@@ -41,9 +44,51 @@ describe('Any Arguments', async() => {
     })
 })
 
+describe('User Arguments', () => {
+    beforeEach(() => {
+        sinon.stub(users, "getByUsername").callsFake(async(username: string, guildId: string, discordIds: string[]) => {
+            let users = [
+                new PolyUser.GuildUser("test1", "FEMME", null, null, "1111"),
+                new PolyUser.DiscordUser("test2", "NEUTRAL", null, null, "111111"),
+                new PolyUser.DiscordUser("test2", "FEMME", null, null, "111112"),
+                new PolyUser.GuildUser("test3", "MASC", null, null, "2222")
+            ]
+
+            let foundUser: PolyUser.User
+            let found = false
+            for (let i = 0; i++; i < users.length) {
+                if (users[i].name === username) {
+                    foundUser = users[i]
+                    found = true
+                }
+            }
+            if(found) {
+                return [foundUser!]
+            }
+            else {
+                throw new ArgumentError("there are no users with that argument", new CommandParser.UserArgument())
+            }
+        })
+    })
+    //TODO: make guild not null
+    it('can retrieve a user', async() => {
+        await assert.isFulfilled(new CommandParser.UserArgument().parse({
+            content: "test1",
+            channel: null as any,
+            guild: null as any,
+            author: null as any
+        }))
+        
+    })
+
+    afterEach(() => {
+        sinon.restore()
+    })
+})
+
 describe('Discord User Arguments', () => {
     let user = new User(client, {
-        id: "<@1234567890>",
+        id: "138790334896275455",
         username: "test user",
         discriminator: "#0000"
     })
@@ -55,7 +100,7 @@ describe('Discord User Arguments', () => {
 
     it('can accept discord users', async() => {
         let userArgument = {
-            content: user.id,
+            content: "<@138790334896275456>",
             channel: null as any,
             guild: null as any,
             author: null as any
@@ -90,6 +135,27 @@ describe('Specific Arguments', () => {
     it('can reject arguments that match neither element', () => {
         assert.isRejected(arg.parse({
             content: "4",
+            channel: null as any,
+            guild: null as any,
+            author: null as any
+        }))
+    })
+})
+
+describe('String Excluded Arguments', () => {
+    let arg = new CommandParser.StringExcludedArgument("excluded")
+    it('can reject arguments with excluded string', () => {
+        assert.isRejected(arg.parse({
+            content: "excluded",
+            channel: null as any,
+            guild: null as any,
+            author: null as any
+        }), CommandParser.ArgumentError)
+    })
+
+    it('can accept string arguments that do not contain excluded string', () => {
+        assert.isFulfilled(arg.parse({
+            content: "included",
             channel: null as any,
             guild: null as any,
             author: null as any
