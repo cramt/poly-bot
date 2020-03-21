a0c93cb7f83608e8a6402bb9383f49076b51ff73
Alexandra Østermark <alex.cramt@gmail.com>
2020-03-21 13:09:25 +0100

    ooopsie doopsie, migrations where bad

--- migrations/2.sql
+++ migrations/2.sql
@@ -11,17 +11,17 @@ DECLARE
 	discord text;
 	guild text;
 BEGIN
-   SELECT users.discord_id, users.guild_id INTO discord, guild FROM users WHERE id = get_topmost_system(NEW.id);
+   SELECT users.discord_id, users.guild_id INTO discord, guild FROM users WHERE id = get_topmost_system(OLD.id);
    DELETE FROM polymap_cache WHERE 
    polymap_cache.guild_id = guild OR
    discord = ANY(polymap_cache.discord_ids);
-   RETURN NEW;
+   RETURN OLD;
 END;
 $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER user_change_polymap_cache_invalidation
-AFTER INSERT OR UPDATE OR DELETE ON public.users
+BEFORE INSERT OR UPDATE OR DELETE ON public.users
 FOR EACH ROW EXECUTE PROCEDURE public.post_user_change_polymap_cache_invalidation();
 
 CREATE OR REPLACE FUNCTION public.post_relationship_change_polymap_cache_invalidation()
@@ -33,37 +33,38 @@ DECLARE
 	right_discord text;
 	right_guild text;
 BEGIN
-   SELECT users.discord_id, users.guild_id INTO left_discord, left_guild FROM users WHERE id = get_topmost_system(NEW.left_user_id);
-   SELECT users.discord_id, users.guild_id INTO right_discord, right_guild FROM users WHERE id = get_topmost_system(NEW.right_user_id);
-   DELETE FROM polymap_cache WHERE
-   (
-	   left_discord = ANY(polymap_cache.discord_ids) 
-	   AND
-	   right_discord = ANY(polymap_cache.discord_ids)
-   )
-   OR
-   (
-	   left_discord = ANY(polymap_cache.discord_ids) 
-	   AND
-	   right_guild = polymap_cache.guild_id
-   )
-   OR
-   (
-	   left_guild = polymap_cache.guild_id
-	   AND
-	   right_guild = polymap_cache.guild_id
-   )
-   OR
-   (
-	   left_guild = polymap_cache.guild_id
-	   AND
-	   right_discord = ANY(polymap_cache.discord_ids)
-   );
-   RETURN NEW;
+
+    SELECT users.discord_id, users.guild_id INTO left_discord, left_guild FROM users WHERE id = get_topmost_system(OLD.left_user_id);
+    SELECT users.discord_id, users.guild_id INTO right_discord, right_guild FROM users WHERE id = get_topmost_system(OLD.right_user_id);
+    DELETE FROM polymap_cache WHERE
+    (
+        left_discord = ANY(polymap_cache.discord_ids) 
+        AND
+        right_discord = ANY(polymap_cache.discord_ids)
+    )
+    OR
+    (
+        left_discord = ANY(polymap_cache.discord_ids) 
+        AND
+        right_guild = polymap_cache.guild_id
+    )
+    OR
+    (
+        left_guild = polymap_cache.guild_id
+        AND
+        right_guild = polymap_cache.guild_id
+    )
+    OR
+    (
+        left_guild = polymap_cache.guild_id
+        AND
+        right_discord = ANY(polymap_cache.discord_ids)
+    );
+    RETURN OLD;
 END;
 $BODY$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER relationship_change_polymap_cache_invalidation
-AFTER INSERT OR UPDATE OR DELETE ON public.relationships
+BEFORE INSERT OR UPDATE OR DELETE ON public.relationships
 FOR EACH ROW EXECUTE PROCEDURE public.post_relationship_change_polymap_cache_invalidation();
--- migrations/3.sql
+++ migrations/3.sql
@@ -1,8 +1,8 @@
 ALTER TABLE relationships
-    ADD CONSTRAINT fk_left_user_id FOREIGN KEY (left_user_id) REFERENCES users (id);
+    ADD CONSTRAINT fk_left_user_id FOREIGN KEY (left_user_id) REFERENCES users (id) ON DELETE CASCADE;
 
 ALTER TABLE relationships
-    ADD CONSTRAINT fk_right_user_id FOREIGN KEY (right_user_id) REFERENCES users (id);
+    ADD CONSTRAINT fk_right_user_id FOREIGN KEY (right_user_id) REFERENCES users (id) ON DELETE CASCADE;
 
 ALTER TABLE users
-    ADD CONSTRAINT fk_system_id FOREIGN KEY (system_id) REFERENCES users(id)
+    ADD CONSTRAINT fk_system_id FOREIGN KEY (system_id) REFERENCES users(id) ON DELETE CASCADE;
