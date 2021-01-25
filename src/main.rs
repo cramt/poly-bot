05c592f6e6c759e4307f12dc6f6dd266f8037646
Alexandra Østermark <alex.cramt@gmail.com>
2021-01-25 22:04:22 +0100

    migration parseing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod migration_constants;
+
+use std::ops::Deref;
+use crate::migration_constants::MIGRATION_FILES;
+
+#[tokio::main]
+async fn main() {
+    println!("{:?}", MIGRATION_FILES.deref())
 }
---
+++ src/migration_constants.rs
@@ -0,0 +1,34 @@
+use include_dir::{include_dir, Dir};
+use std::path::Path;
+use once_cell::sync::Lazy;
+use regex::{Regex, Captures};
+use std::num::ParseIntError;
+use std::string::FromUtf8Error;
+use std::ops::Deref;
+
+const MIGRATION_DIR: Dir = include_dir!("./migrations");
+
+pub const MIGRATION_FILES: Lazy<Vec<(usize, String)>> = Lazy::new(|| {
+    let regex = Regex::new(r"([0-9]+).sql").unwrap();
+    MIGRATION_DIR.files().into_iter().map(|x| {
+        let captures = match regex.captures(x.path().to_str().unwrap()) {
+            None => return None,
+            Some(x) => x
+        };
+        let capture = match captures.get(1) {
+            None => return None,
+            Some(x) => x
+        };
+        let i = match capture.as_str().parse::<usize>() {
+            Ok(x) => x,
+            Err(_) => return None
+        };
+        let content = match String::from_utf8(x.contents().to_vec()) {
+            Ok(x) => x,
+            Err(_) => return None
+        };
+        Some((i, content))
+    }).filter(|x|x.is_some())
+        .map(|x|x.unwrap())
+        .collect::<Vec<(usize, String)>>()
+});
